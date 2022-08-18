315e391ab2aeb364d00ceda6b144566a5bcc6ff2
Ludmi Lopez <ludmit@outlook.com>
2022-08-18 15:21:03 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, {useState, useEffect} from 'react';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import colorsArray from './colorsArray';
+import $ from 'jquery';
 import './style/App.scss'
 
 let quoteJson = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
@@ -18,7 +19,6 @@ function App() {
         const response = await fetch(url)
         const parse = await response.json()
         setQuotesArray(parse.quotes);
-        console.log(parse);
     }
 
     useEffect(() =>{
@@ -32,12 +32,17 @@ function App() {
         setAccentColor(colorsArray[rand])
         setQuote(quotesArray[rand].quote)
         setAuthor(quotesArray[rand].author)
+        $('html body').animate(
+            {
+              backgroundColor: (colorsArray[rand]),
+              color: (colorsArray[rand])
+            },
+            1000);
     }
 
   return (
-  <body style={{backgroundColor:accentColor}}>
-    <>
   
+    <>
     <div className='app'>
         <header className='app-header' id='quote-box' style={{color:accentColor}}>
             <p id='text' className='text'>"{quote}"</p>
@@ -47,7 +52,7 @@ function App() {
         </header>
     </div>
     </>
-    </body>
+    
   )
 }
 
